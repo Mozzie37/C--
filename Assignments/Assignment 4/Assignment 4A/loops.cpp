655f449db9a1421c36b76356b0ef5e83ea05b8a7
Mozzie Osaba <noahosaba@gmail.com>
2023-07-23 22:03:28 -0400

    7/23

---
+++ Assignments/Assignment 4/Assignment 4A/loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cin, std::cout, std::endl;
+
+
+
+
+/*
+1) A loop that might run zero or more times
+2) A loop that will always run at least once
+3) A loop with a predetermined start and end
+*/
+
+
+
+int main(){
+    
+
+
+    return 0;
+}
