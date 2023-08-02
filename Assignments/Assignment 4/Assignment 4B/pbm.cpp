62c98cd79b7877f54de350772ad53db262981f48
Mozzie Osaba <noahosaba@gmail.com>
2023-08-02 17:33:26 -0400

    added PBM file generator

---
+++ Assignments/Assignment 4/Assignment 4B/pbm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout, std::cin;
+int squareSize, borderSize, squareColor, borderColor;
+void userInput(){
+    cout << "Enter the size of the square: ";
+    cin >> squareSize;
+    cout << "Enter the size of the border: ";
+    cin >> borderSize;
+    cout << 
+
+}
+
+
+int main(){
+    userInput();
+    return 0;
+}
